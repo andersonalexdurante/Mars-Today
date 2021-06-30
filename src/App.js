d1bec21eab2d45380a47df576d1a8055736b746e
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2021-06-30 00:52:45 -0300

    adiciona contagem do sol e novas fotos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import { useEffect, useState } from 'react';
 import background from './assets/background.png'
 import mars from './assets/mars.svg'
 import sistemaSolar from './assets/sistema-solar.png'
-
+import api from './services/api'
  
-function App() {
+export function App() {
 
-  console.log(process.env.REACT_APP_API_KEY)
+  const [sol, setSol] = useState(0)
+  const [newPhotos, setNewPhotos] = useState(0)
+
+  useEffect(() => {
+    async function fetchData() {
+      const responseSol = await api.get(`?&api_key=${process.env.REACT_APP_API_KEY}`)
+      setSol(responseSol.data.rover.max_sol)
+
+      const responseLatestPhotos = await api.get(`/latest_photos?api_key=${process.env.REACT_APP_API_KEY}`)
+      setNewPhotos(responseLatestPhotos.data.latest_photos.length)
+      
+      
+    }
+    fetchData()
+  }, [])
 
   return (
     <div className="background" style={{backgroundImage: `url(${background})`}}>
         <div className="nav-bar">
-            <img src={mars} alt="mars-logo"/>
+            <img id="mars" src={mars} alt="mars-logo"/>
             <div>
               <h1>MARS TODAY</h1>
               <h2>BY PERSEVERANCE</h2>
             </div>
-            
             <img src={sistemaSolar} alt="solar-system"/>
         </div>
+        <h1 id="sol">SOL {sol}</h1>
+        <h4>{newPhotos} new photos!</h4>
     </div>
   );
 }
