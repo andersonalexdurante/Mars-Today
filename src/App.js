94f3b9699de0fa67f24baad911cc0d7761043221
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2021-06-30 15:15:40 -0300

    estourei o limite da api

--- src/App.js
+++ src/App.js
@@ -8,23 +8,35 @@ import MissionCard from './components/MissionCard'
 export function App() {
 
   const [sol, setSol] = useState(0)
+  const [earthDate, setEarthDate] = useState(0)
   const [newPhotos, setNewPhotos] = useState(0)
   const [launchDate, setLaunchDate] = useState('')
   const [landingDate, setLandingDate] = useState('')
   const [totalPhotos, setTotalPhotos] = useState(0)
-
+  const [photos, setPhotos] = useState([])
+  
   useEffect(() => {
     async function fetchData() {
       const responseSol = await api.get(`?&api_key=${process.env.REACT_APP_API_KEY}`)
       setSol(responseSol.data.rover.max_sol)
+      setEarthDate(responseSol.data.rover.max_date)
       setLaunchDate(responseSol.data.rover.launch_date)
       setLandingDate(responseSol.data.rover.landing_date)
       setTotalPhotos(responseSol.data.rover.total_photos)
-      const responseLatestPhotos = await api.get(`/latest_photos?api_key=${process.env.REACT_APP_API_KEY}`)
-      setNewPhotos(responseLatestPhotos.data.latest_photos.length)
     }
     fetchData()
   }, [])
+
+  useEffect(() => {
+    async function fetchData() {
+      const responseLatestPhotos = await api.get(`/latest_photos?api_key=${process.env.REACT_APP_API_KEY}`)
+      setNewPhotos(responseLatestPhotos.data.latest_photos.length)
+      responseLatestPhotos.data.latest_photos.forEach(photo => {
+        setPhotos([...photos, photo.img_src])
+      });
+    }
+    fetchData()
+  }, [photos])
 
   return (
     <div className="background" style={{backgroundImage: `url(${background})`}}>
@@ -37,8 +49,13 @@ export function App() {
             <img src={sistemaSolar} alt="solar-system"/>
         </div>
         <h1 id="sol">SOL {sol}</h1>
-        <h4 id="number-photos">{newPhotos} new photos!</h4>
+        <h4 id="number-photos">Earth Date {earthDate} - {newPhotos} new photos!</h4>
         <MissionCard launchDate={launchDate} landingDate={landingDate} totalPhotos={totalPhotos}/>
+        <div>
+          {photos.forEach(photo => {
+            <img src={photo} alt="mars photos"/>
+          })}
+        </div>
     </div>
   );
 }
