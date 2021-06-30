b5167611f8fa5fa0a9884a38bef4c420fcc46930
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2021-06-29 23:21:10 -0300

    implementa nav-bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import background from './assets/background.png'
+import mars from './assets/mars.svg'
+import sistemaSolar from './assets/sistema-solar.png'
 
 function App() {
   return (
-    <div></div>
+    <div className="background" style={{backgroundImage: `url(${background})`}}>
+        <div className="nav-bar">
+            <img src={mars} alt="mars-logo"/>
+            <h1>MARS TODAY</h1>
+            <img src={sistemaSolar} alt="solar-system"/>
+        </div>
+    </div>
   );
 }
 
