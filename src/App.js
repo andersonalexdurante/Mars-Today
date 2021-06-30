d1c2ca5d11a7606a681e14aec8acf12bff29b0b5
Anderson Alex Durante <anderson.alexdurante@gmail.com>
2021-06-30 00:04:20 -0300

    adiciona chamada api com axios

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import background from './assets/background.png'
 import mars from './assets/mars.svg'
 import sistemaSolar from './assets/sistema-solar.png'
 
+ 
 function App() {
+
+  console.log(process.env.REACT_APP_API_KEY)
+
   return (
     <div className="background" style={{backgroundImage: `url(${background})`}}>
         <div className="nav-bar">
             <img src={mars} alt="mars-logo"/>
-            <h1>MARS TODAY</h1>
+            <div>
+              <h1>MARS TODAY</h1>
+              <h2>BY PERSEVERANCE</h2>
+            </div>
+            
             <img src={sistemaSolar} alt="solar-system"/>
         </div>
     </div>
